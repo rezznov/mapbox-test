b216f1005823efdb6cb5fa352b2cdca8adc7504c
rezznov <27864684+rezznov@users.noreply.github.com>
2022-08-07 00:17:57 +0430

    simple mapview testing

--- app/src/main/java/com/reznov/mapbox_test/MainActivity.kt
+++ app/src/main/java/com/reznov/mapbox_test/MainActivity.kt
@@ -2,10 +2,36 @@ package com.reznov.mapbox_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.reznov.mapbox_test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var _binding: ActivityMainBinding? = null
+    val binding: ActivityMainBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater, null, false)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
     }
 }
